55f74bb5856043a33f7aead50853189f11276d99
soyul <syuul3139@gmail.com>
2023-07-05 13:34:40 +0900

    todolist

---
+++ React/To_Do_List/src/pages/date.js
@@ -0,0 +1,10 @@
+
+const Date = () =>{
+    return(
+        <>
+            <h1>날짜 요소를 보여주는 페이지입니다</h1>
+        </>
+    );
+}
+
+export default Date;
---
+++ React/To_Do_List/src/pages/shape.js
@@ -0,0 +1,10 @@
+
+const Shape = () => {
+    return(
+        <>
+            <h1>모양을 담아주는 페이지</h1>
+        </>
+    );
+}
+
+export default Shape;
